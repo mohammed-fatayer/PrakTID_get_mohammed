fa02c127574c9fa113e5dadd91922c6b6a191aca
Hakan GENÇOĞLU <hakan.gencoglu@izu.edu.tr>
2023-02-26 22:12:41 +0300

    bugfixes 26022023

--- lib/view/Login.dart
+++ lib/view/Login.dart
@@ -5,6 +5,10 @@ import 'package:praktid/view/register.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:get/get.dart';
 //import 'package:google_sign_in/google_sign_in.dart';
+import 'package:praktid/view/mainPage.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 
 class LoginPage extends StatelessWidget {
